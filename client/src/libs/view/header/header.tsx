4d0ce7762d142f1ea89642c40edbf5d6f6a20ef4
HubaBuba2000 <hubabuba20017@gmail.com>
2025-11-02 21:18:12 +0300

    final final final

--- client/src/libs/view/header/header.tsx
+++ client/src/libs/view/header/header.tsx
@@ -8,7 +8,7 @@ export const Header = ({
 
     return (
         <div className="fixed flex justify-between top-0 w-full border py-6 shadow-sm z-50 bg-white p-4">
-            <p className="uppercase font-bold text-xl" >Платформа</p>
+            <p className="uppercase font-bold text-xl" >APC</p>
             {children}
         </div>
     )
