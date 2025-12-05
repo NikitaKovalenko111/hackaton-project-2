d57c1edfbb9b9a655d2fa604cd8fdcc382c5fad5
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2025-12-05 20:36:54 +0500

    fix

--- server/src/EmployeeModule/employee.controller.ts
+++ server/src/EmployeeModule/employee.controller.ts
@@ -125,7 +125,7 @@ export class EmployeeController {
 
       const file = createReadStream(imagePath)
 
-      res.appendHeader('Content-Type', `image/${employee.employee_photo.split(".")[1]}`)
+      res.header('Image-Type', `image/${employee.employee_photo.split(".")[1]}`)
 
       return new StreamableFile(file, {
         "type": `image/${employee.employee_photo.split(".")[1]}`
